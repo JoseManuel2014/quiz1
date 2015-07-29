217dd4c01f029dca6cc9e2c3a4d52f9faee1dcc2
JoseManuel2014 <jmbarrio@gmail.com>
2015-07-29 17:45:30 -0500

    tratando de arreglar

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -40,5 +40,4 @@ exports.answer = function(req,res) {
 			res.render('quizes/answer', {respuesta: 'Incorrecto'});
 		}
 	})
-
 };
--- routes/index.js
+++ routes/index.js
@@ -19,17 +19,15 @@ module.exports = router;
 Modifique el router (routers/index.js) para que atienda las peticiones "GET /author" 
 y sirva una nueva vista views/author.ejs con los datos de los autores o autor de la página, 
 mostrando el nombre de los autores, su fotografía y un pequeño video (opcional) de 30 seg.
-*/
+
 
 router.get('/autor', function(req, res) {
   res.render('/autor', /views/author.ejs);
 });
+*/
 
 //render('/autor', /views/author.ejs);
-
-module.exports = router;
 
 router.get('/author', quizController.author);
 
 module.exports = router;
-
