ea6653a927e2d9e04b3f83f8f931a9112c8fa3b1
JoseManuel2014 <jmbarrio@gmail.com>
2015-07-29 23:12:19 -0500

    arreglado

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,21 +1,3 @@
-//GET /author
-exports.author = function(req, res){
-	res.render('author', {autor: 'Isabel Ríos'})
-}
-
-//GEt /quizes/question
-exports.question = function(req, res){
-	res.render('quizes/question', {pregunta: 'Capital de Italia'});
-};
-
-//GEt /quizes/answer
-exports.answer = function (req, res){
-	if (req.query.respuesta ==='Roma'){
-		res.render('quizes/answer', {respuesta: 'Correcto'});
-	} else {
-		res.render('quizes/answer', {respuesta: 'Incorrecto'});
-	}
-
 var models = require('../models/models.js')
 
 //GET /author
@@ -28,7 +10,6 @@ exports.question = function(req,res) {
 	models.Quiz.findAll().then(function(quiz) {
 		res.render('quizes/question', {pregunta: quiz[0].pregunta});
 	})
-	
 };
 
 //GEt /quizes/answer
